4e902b5a20c41623ceb5077ef0c435fa2e314769
khinmarwin087-oss <khinmarwin087@gmail.com>
2026-01-28 07:52:49 +0630

    Update and rename menu js to menu.js

---
+++ menu.js
@@ -0,0 +1,31 @@
+const menuData = [
+    {
+        id: 1,
+        name: "Vintage Oversized Tee",
+        price: 15000,
+        description: "High quality cotton fabric.",
+        images: [
+            "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?w=500",
+            "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?w=500",
+            "https://images.unsplash.com/photo-1562157873-818bc0726f68?w=500",
+            "https://images.unsplash.com/photo-1576566588028-4147f3842f27?w=500",
+            "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?w=500",
+            "https://images.unsplash.com/photo-1554568218-0f1715e72254?w=500"
+        ]
+    },
+    {
+        id: 2,
+        name: "Casual Shirt",
+        price: 22000,
+        description: "Perfect for daily wear.",
+        images: [
+            "https://images.unsplash.com/photo-1596755094514-f87034a26cc1?w=500",
+            "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?w=500",
+            "https://images.unsplash.com/photo-1621072156002-e2fcc103e86e?w=500",
+            "https://images.unsplash.com/photo-1603252109303-2751441dd15e?w=500",
+            "https://images.unsplash.com/photo-1604644401890-0bd678c83788?w=500",
+            "https://images.unsplash.com/photo-1598033129183-c4f50c7176c8?w=500"
+        ]
+    }
+    // အစ်ကို့စိတ်ကြိုက် နောက်ထပ် ၃ ခု ထပ်တိုးပါ...
+];
